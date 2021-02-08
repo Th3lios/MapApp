375cd0abfee4c6325fb0b0c262fec834aa772c6b
Elias Araya <earayad@hotmail.cl>
2021-02-08 19:21:26 -0300

    apk path added

--- __tests__/simple.test.js
+++ __tests__/simple.test.js
@@ -6,12 +6,13 @@ const config = {
     browserName: '',
     appiumVersion: '1.19.1',
     platformName: 'Android',
-    platformVersion: '10',
-    deviceName: 'HUAWEI P30 lite',
+    //platformVersion: '10',
+    deviceName: 'foo',
     automationName: 'UiAutomator2',
-    appPackage: "com.mapapp",
-    appActivity: "com.mapapp.MainActivity",
-    udid: "AGT4C19513002563"
+    app: '/Volumes/Samsung_T5/Projects/React Native/MapApp/android/app/build/outputs/apk/debug/app-debug.apk'
+    //appPackage: "com.mapapp",
+    //appActivity: "com.mapapp.MainActivity",
+    //udid: "AGT4C19513002563"
 }
 
 const driver = wd.promiseChainRemote('localhost', PORT);
